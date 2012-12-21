e4688fb6fe48b12e391b62d82281f3a039207a8f
Tomas Doran <tomas.doran@timgroup.com>
2012-12-21 11:39:15 +0000

    Fix alignment to make things easier to read

--- lib/orc/mismatch_resolver.rb
+++ lib/orc/mismatch_resolver.rb
@@ -2,6 +2,17 @@ require 'orc/actions'
 
 class Orc::MismatchResolver
   def in_case(state, name)
+    state.keys.each { |k|
+      if state[k].nil?
+        t_state = state.clone
+        f_state = state.clone
+        t_state[k] = true
+        f_state[k] = false
+        in_case(t_state, name)
+        in_case(f_state, name)
+        return
+      end
+    }
     @cases[state] = lambda {|instance| Orc::Action.const_get(name).new(@remote_client,instance)}
   end
 
@@ -9,49 +20,49 @@ class Orc::MismatchResolver
     @remote_client = remote_client
     @cases = {}
     in_case({
-      :should_participate=>true,
-      :does_participate=>true,
-      :version_mismatch=>false
+      :should_participate => true,
+      :does_participate   => true,
+      :version_mismatch   => false
     }, 'ResolvedCompleteAction')
     in_case({
-      :should_participate=>false,
-      :does_participate=>false,
-      :version_mismatch=>false
+      :should_participate => false,
+      :does_participate   => false,
+      :version_mismatch   => false
     }, 'ResolvedCompleteAction')
     in_case({
-      :should_participate=>false,
-      :does_participate=>false,
-      :version_mismatch=>true
+      :should_participate => false,
+      :does_participate   => false,
+      :version_mismatch   => true
     }, 'UpdateVersionAction')
     in_case({
-      :should_participate=>true,
-      :does_participate=>true,
-      :version_mismatch=>true
+      :should_participate => true,
+      :does_participate   => true,
+      :version_mismatch   => true
     }, 'DisableParticipationAction')
     in_case({
-      :should_participate=>false,
-      :does_participate=>false,
-      :version_mismatch=>true
+      :should_participate => false,
+      :does_participate   => false,
+      :version_mismatch   => true
     }, 'UpdateVersionAction')
     in_case({
-      :should_participate=>true,
-      :does_participate=>false,
-      :version_mismatch=>true
+      :should_participate => true,
+      :does_participate   => false,
+      :version_mismatch   => true
     }, 'UpdateVersionAction')
     in_case({
-      :should_participate=>true,
-      :does_participate=>false,
-      :version_mismatch=>false
+      :should_participate => true,
+      :does_participate   => false,
+      :version_mismatch   => false
     }, 'EnableParticipationAction')
     in_case({
-      :should_participate=>false,
-      :does_participate=>true,
-      :version_mismatch=>false
+      :should_participate => false,
+      :does_participate   => true,
+      :version_mismatch   => false
     }, 'DisableParticipationAction')
     in_case({
-      :should_participate=>false,
-      :does_participate=>true,
-      :version_mismatch=>true
+      :should_participate => false,
+      :does_participate   => true,
+      :version_mismatch   => true
     }, 'DisableParticipationAction')
   end
 
