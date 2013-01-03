9055baeeb2d363b913378fef905a6a3ec1746f5e
Tomas Doran <tomas.doran@timgroup.com>
2013-01-03 16:12:07 +0000

    Revert "Cleanup the horrible code"
    This reverts commit aa06496346a875f7c155e5d381a772c14325c1eb.

--- lib/orc/mismatch_resolver.rb
+++ lib/orc/mismatch_resolver.rb
@@ -2,38 +2,22 @@ require 'orc/actions'
 
 class Orc::MismatchResolver
   def in_case(state, name)
-    multiply_missing_states(state).each do |state|
-      add_case(state, name)
-    end
-  end
-
-  def multiply_missing_states(original_state)
-    all_dimensions = [
+    [
       :should_participate,
       :does_participate,
       :version_mismatch,
       :is_healthy,
-    ]
-
-    new_states = []
-    all_dimensions.each do |dimension|
-      unless original_state.has_key? dimension
-        [true,false].each do |value|
-          multiplied_state = original_state.clone
-          multiplied_state[dimension] = value
-          new_states.push multiplied_state
-        end
+    ].each { |k|
+      if state[k].nil?
+        t_state = state.clone
+        f_state = state.clone
+        t_state[k] = true
+        f_state[k] = false
+        in_case(t_state, name)
+        in_case(f_state, name)
+        return
       end
-    end
-
-    if new_states.size
-      new_states.each {|s| multiply_missing_states(s) { |x| yield x } }
-    else
-      yield original_state
-    end
-  end
-
-  def add_case(state, name)
+    }
     @cases[state] = lambda {|instance| Orc::Action.const_get(name).new(@remote_client,instance)}
   end
 
