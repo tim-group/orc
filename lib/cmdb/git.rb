bef2b3c078a897b3f2bfc48533d28128846c573b
Tomas Doran <tomas.doran@timgroup.com>
2013-01-03 13:56:05 +0000

    Add a timeout to cmdb pushes / pulls.
    Ruby doesn't seem to expose the POSIX alarm(), ergo I did a nasty and dlloaded it from libc.
    Other suggestions on how to do this more sensibly welcome, but I can't think of any that don't
    involve re-writing significant chunks of the ruby git library, or doing nasty things like forking
    a sub process which then sends the SIGALRM if it isn't killed within N seconds...

--- lib/cmdb/git.rb
+++ lib/cmdb/git.rb
@@ -2,14 +2,28 @@ require 'cmdb/namespace'
 require 'git'
 require 'logger'
 
+require 'dl/import'
+module Alarm
+  extend DL::Importable
+  if RUBY_PLATFORM =~ /darwin/
+    so_ext = 'dylib'
+  else
+    so_ext = 'so.6'
+  end
+  dlload "libc.#{so_ext}"
+  extern "unsigned int alarm(unsigned int)"
+end
+
 class CMDB::Git
   def initialize(options={})
     @repo_url = options[:origin] || 'git@git:cmdb'
     @local_path = options[:local_path] || '/opt/orctool/data/cmdb/'
     @branch = options[:branch] || "master"
+    @timeout = 10
   end
 
   def update()
+    Alarm.alarm(@timeout)
     if File.directory?(@local_path)
       @git = Git.open(@local_path, :log => Logger.new(STDOUT))
       @git.remotes.first.fetch
@@ -23,6 +37,7 @@ class CMDB::Git
     if self.get_branch() != @branch
       @git.branch( @branch ).checkout
     end
+    Alarm.alarm(0)
   end
 
   def get_branch()
@@ -32,13 +47,16 @@ class CMDB::Git
   def commit_and_push()
     if File.directory?(@local_path)
       if (@git.status.changed.size>0)
+        Alarm.alarm(@timeout)
         @git.commit_all('orc auto-updating cmdb')
         @git.fetch( 'origin' )
         @git.merge('origin', 'merge concurrent modifications')
         @git.push()
+        Alarm.alarm(0)
       end
     else
       raise "#{@local_path} doesn't exist"
     end
   end
 end
+
