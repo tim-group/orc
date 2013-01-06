842c9a1b88522972c6512bf8da10504fabdebc55
Tomas Doran <bobtfish@bobtfish.net>
2013-01-06 03:25:57 +0000

    Go back to storing instances
    Rather than actions. This removed a whole load of code we're not using
    any more

--- lib/orc/model/application.rb
+++ lib/orc/model/application.rb
@@ -5,11 +5,10 @@ require 'orc/model/group'
 require 'orc/model/instance'
 
 class Orc::Model::Application
-
+  attr_reader :instances
   def initialize(args)
     @remote_client = args[:remote_client]
     @cmdb = args[:cmdb]
-    @instance_actions = {}
     @environment = args[:environment] || raise('Must pass environment')
     @application = args[:application] || raise('Must pass application')
     @mismatch_resolver = args[:mismatch_resolver] || raise('Must pass mismatch resolver')
@@ -39,24 +38,11 @@ class Orc::Model::Application
       raise Orc::Exception::GroupMissing.new("#{instance[:group]}") if group.nil?
       instance_models << Orc::Model::Instance.new(instance, group)
     end
-    instance_models.sort_by { |instance| instance.group_name }
-  end
-
-  def instances
-    actions = @instance_actions.values || raise("We have not yet calculated the instances")
-    instances = []
-    actions.each { |l| a = l[-1]; instances << a.instance if a != nil }
-    instances
+    @instances = instance_models.sort_by { |instance| instance.group_name }
   end
 
   def participating_instances
     instances.reject { |instance| not instance.participation }
-  end
-
-  def has_failed_actions(action)
-    return false if @instance_actions[action.key].nil?
-    failed_actions = @instance_actions[action.key].reject { |action| not action.failed? }
-    failed_actions.size > 0
   end
 
   def get_proposed_resolutions_for(live_instances)
@@ -79,15 +65,8 @@ class Orc::Model::Application
       proposed_resolutions.each { |r| @progress_logger.log("    #{r.class.name} on #{r.host} group #{r.group_name}") }
     end
 
-    proposed_resolutions.each do |action|
-      if @instance_actions[action.key].nil?
-        @instance_actions[action.key] = []
-      end
-      @instance_actions[action.key].push action
-    end
-
     useable_resolutions = proposed_resolutions.reject { |resolution|
-      resolution.complete? or has_failed_actions(resolution)
+      resolution.complete?
     }
 
     useable_resolutions
