c24233f903180e5c068d418b4c6b4fb816a8ad6f
Tomas Doran <bobtfish@bobtfish.net>
2013-01-18 14:15:37 -0500

    Failing test for looping over deploying an app

--- spec/orc/actions_spec.rb
+++ spec/orc/actions_spec.rb
@@ -29,3 +29,34 @@ describe Orc::Action::WaitActionBase do
   end
 end
 
+describe Orc::Action::UpdateVersionAction do
+  def get_testaction(group='A')
+    group = double()
+    group.stub(:name).and_return(group)
+    group.stub(:target_version).and_return(1.1)
+    instance = double()
+    instance.stub(:group).and_return(group)
+    instance.stub(:host).and_return('localhost')
+    remote_client = double()
+    remote_client.stub(:update_to_version).and_return(true)
+    Orc::Action::UpdateVersionAction.new(remote_client, instance)
+  end
+
+  it 'works as expected' do
+    i = get_testaction
+    i.do_execute([i]).should eql(true)
+  end
+
+  it 'works as expected for two actions in turn in different groups' do
+    first = get_testaction()
+    second = get_testaction('B')
+    second.do_execute([first, second]).should eql(true)
+  end
+
+  it 'throws an exception if the same action for the same group is run twice' do
+    first = get_testaction()
+    second = get_testaction()
+    expect { second.do_execute([first, second]) }.to raise(Orc::Exception::FailedToResolve)
+  end
+end
+
