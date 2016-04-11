814c08597c1366300a551642d028800de566410f
Graham Allan <graham.allan@timgroup.com>
2016-04-11 16:25:34 +0100

    Remove the help about rolling-restart, since it's not implemented.
    (also remove test)

--- spec/orc/util_option_parser_spec.rb
+++ spec/orc/util_option_parser_spec.rb
@@ -124,14 +124,6 @@ describe Orc::Util::OptionParser do
     expect(command.class.name).to eql('Orc::Util::OptionParser::DeployRequest')
   end
 
-  it 'Works for RollingRestartRequest' do
-    parser = MockOptionParser.new(['--rolling-restart', '--environment', 'bar', '--application', 'MyApp',
-                                   '--group', 'blue']).parse
-
-    expect(parser.commands.size).to eql(1)
-    command = parser.commands[0]
-    expect(command.class.name).to eql('Orc::Util::OptionParser::RollingRestartRequest')
-  end
   it 'will exit 1 when invalid option provided' do
     begin
       MockOptionParser.new(['--socks', '--environment', 'bar', '--application', 'MyApp', '--group', 'blue']).parse
