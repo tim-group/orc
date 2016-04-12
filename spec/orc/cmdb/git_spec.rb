83770d4b27a8a9e769caceae1703ecc0dd31f18d
Graham Allan <graham.allan@timgroup.com>
2016-04-12 22:39:46 +0100

    Various test cleanup.

--- spec/orc/cmdb/git_spec.rb
+++ spec/orc/cmdb/git_spec.rb
@@ -7,19 +7,16 @@ describe Orc::CMDB::Git do
     Git.open(@local, :log => nil)
   end
 
-  def setup_alice_and_bob_repos
-    FileUtils.touch("#{@origin}/alice")
-    FileUtils.touch("#{@origin}/bob")
-    @repo.add("#{@origin}/alice")
-    @repo.add("#{@origin}/bob")
+  def setup_alice_and_bob_clones_of_origin
+    FileUtils.touch("#{@origin}/alice_file")
+    FileUtils.touch("#{@origin}/bob_file")
+    @repo.add("#{@origin}/alice_file")
+    @repo.add("#{@origin}/bob_file")
     @repo.commit_all('Initial commit')
     @repo.config('core.bare', 'true')
 
-    alice_path = @tempdir + '/alice_clone'
-    bob_path = @tempdir + '/bob_clone'
-
-    bob = Orc::CMDB::Git.new(:local_path => bob_path, :origin => @origin)
-    alice = Orc::CMDB::Git.new(:local_path => alice_path, :origin => @origin)
+    bob = Orc::CMDB::Git.new(:local_path => "#{@tempdir}/bob_clone", :origin => @origin)
+    alice = Orc::CMDB::Git.new(:local_path => "#{@tempdir}/alice_clone", :origin => @origin)
     bob.update
     alice.update
 
@@ -27,7 +24,7 @@ describe Orc::CMDB::Git do
   end
 
   before do
-    @tempdir = Dir.mktmpdir(nil, "/tmp")
+    @tempdir = Dir.mktmpdir(nil, '/tmp')
     @origin = "#{@tempdir}/cmdb_origin"
     @repo = Git.init(@origin)
     FileUtils.touch("#{@origin}/file")
@@ -35,9 +32,9 @@ describe Orc::CMDB::Git do
     @repo.add("#{@origin}/file")
     @repo.add("#{@origin}/file2")
     @repo.commit_all('Initial commit')
-    @repo.config("core.bare", "true")
-    @local = @tempdir + "/cmdb"
-    @second_copy_of_local = @tempdir + "/cmdb2"
+    @repo.config('core.bare', 'true')
+    @local = @tempdir + '/cmdb'
+    @second_copy_of_local = "#{@tempdir}/cmdb2"
   end
 
   after do
@@ -47,7 +44,7 @@ describe Orc::CMDB::Git do
   it 'pulls or clones the cmdb' do
     gitcmdb = Orc::CMDB::Git.new(:local_path => @local, :origin => @origin)
     gitcmdb.update
-    expect(gitcmdb.get_branch).to eql("master")
+    expect(gitcmdb.get_branch).to eql('master')
   end
 
   it 'try to commit to a repo without updating it will fail' do
@@ -60,7 +57,7 @@ describe Orc::CMDB::Git do
   end
 
   it 'can clone a specific branch' do
-    branch_name = "mybranch"
+    branch_name = 'mybranch'
     gitcmdb = Orc::CMDB::Git.new(
       :local_path => @local,
       :origin => @origin,
@@ -77,8 +74,8 @@ describe Orc::CMDB::Git do
     )
     gitcmdb.update
 
-    File.open(@local + '/file', "w") do |f|
-      f.write("hello world")
+    File.open(@local + '/file', 'w') do |f|
+      f.write('hello world')
     end
 
     gitcmdb.commit_and_push
@@ -89,7 +86,7 @@ describe Orc::CMDB::Git do
     )
     gitcmdb2.update
 
-    expect(IO.read(@second_copy_of_local + '/file')).to eql("hello world")
+    expect(IO.read(@second_copy_of_local + '/file')).to eql('hello world')
   end
 
   it 'correctly merges changes when the current repo has fast-forward commits' do
@@ -103,15 +100,15 @@ describe Orc::CMDB::Git do
       :origin => @origin
     )
     gitcmdb2.update
-    File.open(@local + '/file', "w") do |f|
-      f.write("hello world")
+    File.open(@local + '/file', 'w') do |f|
+      f.write('hello world')
     end
     gitcmdb.commit_and_push
-    File.open(@second_copy_of_local + '/file2', "w") do |f|
-      f.write("hello world")
+    File.open(@second_copy_of_local + '/file2', 'w') do |f|
+      f.write('hello world')
     end
     gitcmdb2.commit_and_push
-    expect(IO.read(@second_copy_of_local + '/file')).to eql("hello world")
+    expect(IO.read(@second_copy_of_local + '/file')).to eql('hello world')
   end
 
   it 'doesnt break if we commit the same tree twice' do
@@ -121,14 +118,14 @@ describe Orc::CMDB::Git do
     )
     gitcmdb.update
 
-    File.open(@local + '/file', "w") do |f|
-      f.write("hello world")
+    File.open(@local + '/file', 'w') do |f|
+      f.write('hello world')
     end
     gitcmdb.commit_and_push
 
     gitcmdb.commit_and_push
 
-    expect(gitcmdb.get_branch).to eql("master")
+    expect(gitcmdb.get_branch).to eql('master')
 
     g = local_git
     commits = g.log
@@ -144,17 +141,17 @@ describe Orc::CMDB::Git do
     )
     gitcmdb.update
 
-    File.open(@local + '/file', "w") do |f|
-      f.write("hello world")
+    File.open(@local + '/file', 'w') do |f|
+      f.write('hello world')
     end
     gitcmdb.commit_and_push
 
-    File.open(@local + '/file', "w") do |f|
-      f.write("hello mars")
+    File.open(@local + '/file', 'w') do |f|
+      f.write('hello mars')
     end
     gitcmdb.commit_and_push
 
-    expect(gitcmdb.get_branch).to eql("master")
+    expect(gitcmdb.get_branch).to eql('master')
 
     g = local_git
     commits = g.log
@@ -171,17 +168,17 @@ describe Orc::CMDB::Git do
     )
     gitcmdb.update
 
-    File.open(@local + '/file', "w") do |f|
-      f.write("hello world")
+    File.open(@local + '/file', 'w') do |f|
+      f.write('hello world')
     end
     gitcmdb.commit_and_push
 
-    File.open(@local + '/file', "w") do |f|
-      f.write("hello mars")
+    File.open(@local + '/file', 'w') do |f|
+      f.write('hello mars')
     end
     gitcmdb.commit_and_push
 
-    expect(gitcmdb.get_branch).to eql("master")
+    expect(gitcmdb.get_branch).to eql('master')
 
     g = local_git
     commits = g.log
@@ -197,16 +194,16 @@ describe Orc::CMDB::Git do
       :origin => @origin
     )
     gitcmdb.update
-    expect(gitcmdb.get_branch).to eql("master")
+    expect(gitcmdb.get_branch).to eql('master')
     gitcmdb.update
-    expect(gitcmdb.get_branch).to eql("master")
+    expect(gitcmdb.get_branch).to eql('master')
   end
 
   it 'retries to push if there have been concurrent modifications after the initial fetching' do
-    alice, bob = setup_alice_and_bob_repos
+    alice, bob = setup_alice_and_bob_clones_of_origin
 
-    File.open(bob.local_path + '/bob', 'w') { |f| f.write("bob's edit") }
-    File.open(alice.local_path + '/alice', 'w') { |f| f.write("alice's edit") }
+    File.open("#{bob.local_path}/bob_file", 'w')  { |f| f.write('bob woz here') }
+    File.open("#{alice.local_path}/alice_file", 'w') { |f| f.write('alice woz here') }
 
     alice_thread = Thread.new { alice.commit_and_push }
     bob_thread = Thread.new { bob.commit_and_push }
@@ -217,50 +214,48 @@ describe Orc::CMDB::Git do
     alice.update
     bob.update
 
-    expect(IO.read(bob.local_path + '/alice')).to eql("alice's edit")
-    expect(IO.read(alice.local_path + '/bob')).to eql("bob's edit")
+    expect(IO.read("#{bob.local_path}/alice_file")).to eql('alice woz here')
+    expect(IO.read("#{alice.local_path}/bob_file")).to eql('bob woz here')
   end
 
   it 'will attempt to retry push if rejected with a git error' do
-    alice, bob = setup_alice_and_bob_repos
+    alice, bob = setup_alice_and_bob_clones_of_origin
 
-    File.open(alice.local_path + '/alice', 'w') { |f| f.write("alice's edit") }
+    File.open("#{alice.local_path}/alice_file", 'w') { |f| f.write('alice woz here') }
 
     number_of_attempts_to_reject = 4
+    reject_number_of_attempts_hook = <<-EOF.gsub(/^\s{6}/, '')
+      #!/bin/sh
+      attempts=`cat #{@origin}/attempts`
+      if [ "$attempts" -lt #{number_of_attempts_to_reject} ] ; then
+        echo $(( attempts + 1 )) > "#{@origin}/attempts"
+        exit 1 # Reject push on the first attempts
+      else
+        exit 0 # Eventually allow push
+      fi
+    EOF
     File.open("#{@origin}/attempts", 'w') { |f| f.write('0') }
     hook_path = "#{@origin}/.git/hooks/pre-receive"
-    hook = %(#!/bin/sh
-attempts=`cat #{@origin}/attempts`
-if [ "$attempts" -lt #{number_of_attempts_to_reject} ] ; then
-  echo $(( attempts + 1 )) > "#{@origin}/attempts"
-  exit 1 # Reject push on the first attempts
-else
-  exit 0 # Eventually allow push
-fi
-)
     FileUtils.touch(hook_path)
     FileUtils.chmod('+x', hook_path)
-    File.open(hook_path, 'w') do |f|
-      f.write(hook)
-    end
+    File.open(hook_path, 'w') { |f| f.write(reject_number_of_attempts_hook) }
 
     alice.commit_and_push
     bob.update
-    expect(IO.read(bob.local_path + '/alice')).to eql("alice's edit")
+    expect(IO.read("#{bob.local_path}/alice_file")).to eql('alice woz here')
   end
 
   it 'will stop retries of push and fail if rejected too many times' do
-    alice, _bob = setup_alice_and_bob_repos
+    alice, _bob = setup_alice_and_bob_clones_of_origin
 
-    File.open(alice.local_path + '/alice', 'w') { |f| f.write("alice's edit") }
+    File.open("#{alice.local_path}/alice_file", 'w') { |f| f.write('alice woz here') }
 
-    File.open("#{@origin}/attempts", 'w') { |f| f.write('0') }
     hook_path = "#{@origin}/.git/hooks/pre-receive"
-    hook = "#!/bin/sh\nexit 1 # Reject every push\n"
     FileUtils.touch(hook_path)
     FileUtils.chmod('+x', hook_path)
     File.open(hook_path, 'w') do |f|
-      f.write(hook)
+      f.write("#!/bin/sh\n"\
+              "exit 1 # Reject every push\n")
     end
 
     expect { alice.commit_and_push }.to raise_error(Git::GitExecuteError)
