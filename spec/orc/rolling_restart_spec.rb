10175eddcd982d0962d7ae9d2873ef17afbd6309
Graham Allan <graham.allan@timgroup.com>
2017-03-15 11:24:55 +0000

    gallan: remove test cases that no longer apply when rolling-restart refuses to run unless everything is already hunky dory. Also the ignored test that is out of date.

--- spec/orc/rolling_restart_spec.rb
+++ spec/orc/rolling_restart_spec.rb
@@ -4,47 +4,6 @@ require 'orc/testutil/in_memory_cmdb'
 require 'orc/testutil/fake_remote_client'
 
 describe Orc::Engine do
-  xit 'sends a restart message to each given host sequentially' do
-    factory = Orc::Factory.new({ :environment => 'a', :application => 'app', :timeout => 0 },
-                               :remote_client => FakeRemoteClient.new(:instances => [
-                                 { :group => 'blue',
-                                   :host => 'h1',
-                                   :version => '5',
-                                   :application => 'app',
-                                   :participating => true,
-                                   :health        => 'healthy' },
-                                 { :group => 'blue',
-                                   :host => 'h2',
-                                   :version => '5',
-                                   :application => 'app',
-                                   :participating => true,
-                                   :health        => 'healthy' }]),
-                               :cmdb => InMemoryCmdb.new(
-                                 :groups => {
-                                   'a-app' => [{
-                                     :name => 'blue',
-                                     :target_participation => true,
-                                     :target_version => '5'
-                                   }]
-                                 }))
-
-    expect(factory.engine.rolling_restart).to eql ['DisableParticipationAction: on h1 blue',
-                                                   'RestartAction: on h1 blue',
-                                                   'EnableParticipationAction: on h1 blue',
-                                                   'DisableParticipationAction: on h2 blue',
-                                                   'RestartAction: on h2 blue',
-                                                   'EnableParticipationAction: on h2 blue']
-  end
-
-  it 'does not allow too few instances to participate' do
-    # has to call status to find out how many apps are participating
-  end
-
-  it 'restarts and reparticipates hosts which are not participating first' do
-    # TODO: discuss with Michal whether we want this behaviour or just fail if the apps need resolved
-    # partition hosts by participation
-    # invoke restart on non-participating instances first
-  end
 
   it 'fails with an error message if the application group is not in the expected state' do
     app_in_unresolved_state = {
